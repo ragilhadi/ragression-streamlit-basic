ba015130b3ac195ef2d1e18f7beee7b412bd55f7
ragilhadi <ragilprasetyo310@gmail.com>
2023-05-07 19:49:48 +0700

    intialize commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+st.title('Simple Regression Example : Model Deployment')
+
+if 'model' not in st.session_state:
+    model = pickle.load(open('data/model.sav', 'rb'))
+    st.session_state['model'] = model
+
+st.write(st.session_state)
+
+area_input = st.number_input('Insert Area of the House :house:')
+bedroom_input = st.number_input('Insert How many bedrooms in the House :bed:')
+age_input = st.number_input('Insert Age of the House :1234:')
+
+if st.button('Model Predict'):
+    data = np.array([area_input, bedroom_input, age_input]).reshape(1,-1)
+    result = st.session_state['model'].predict(data)
+    st.write(f'Prediction model : ${result[0]}')
+
+else:
+    st.write('Please input the feature above to start modelling')
+
